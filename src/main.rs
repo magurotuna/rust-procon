f0c45117f6cafa51c3baf252c406cc9fe0d4ea58
Yusuke Tanaka <wing0920@gmail.com>
2019-08-11 17:12:00 +0900

    stepby の独自実装など

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![allow(unused_imports)]
 #![allow(non_snake_case)]
 #![allow(dead_code)]
-#[allow(unused_macros)]
+#![allow(unused_macros)]
+
 use std::cell::RefCell;
 use std::cmp::{max, min, Ordering};
 use std::collections::*;
@@ -10,6 +11,7 @@ use std::io::{stderr, stdin, BufRead, Write};
 use std::mem::{replace, swap};
 use std::ops::*;
 use std::rc::Rc;
+use std::usize;
 
 macro_rules! read {
     ([$t:ty] ; $n:expr) =>
@@ -33,13 +35,93 @@ fn rl() -> String {
     buf.trim_right().to_owned()
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// 飛ばし飛ばしで値を使うイテレータ Rust v1.28以降でしか使えないため独自実装
+/// 簡易実装のためインデックスのオーバーフロー等は考慮していない
+pub struct StepBy<I> {
+    iter: I,
+    step: usize,
+    first_take: bool,
+}
+impl<I> StepBy<I> {
+    pub fn new(iter: I, step: usize) -> StepBy<I> {
+        StepBy {
+            iter: iter,
+            step: step - 1,
+            first_take: true,
+        }
+    }
+}
+impl<I> Iterator for StepBy<I>
+where
+    I: Iterator,
+{
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.first_take {
+            self.first_take = false;
+            self.iter.next()
+        } else {
+            self.iter.nth(self.step)
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let inner_hint = self.iter.size_hint();
+        if self.first_take {
+            let f = |n| {
+                if n == 0 {
+                    0
+                } else {
+                    1 + (n - 1) / (self.step + 1)
+                }
+            };
+            (f(inner_hint.0), inner_hint.1.map(f))
+        } else {
+            let f = |n| n / (self.step + 1);
+            (f(inner_hint.0), inner_hint.1.map(f))
+        }
+    }
+}
+impl<I> ExactSizeIterator for StepBy<I> where I: ExactSizeIterator {}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 trait IteratorExt: Iterator + Sized {
+    // ref: https://qiita.com/vain0x/items/512784ff60ce599dccae#vec
     fn vec(self) -> Vec<Self::Item> {
         self.collect()
+    }
+
+    // step_by は v1.28.0 以降でしか使えないため独自実装
+    fn stepby(self, step: usize) -> StepBy<Self>
+    where
+        Self: Sized,
+    {
+        StepBy::new(self, step)
     }
 }
 
 impl<T: Iterator> IteratorExt for T {}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// 逆順ソート ReverseがRust v1.19以降でしか使えないため独自実装
+#[derive(Eq, PartialEq, Clone, Debug)]
+pub struct Rev<T>(pub T);
+
+impl<T: PartialOrd> PartialOrd for Rev<T> {
+    fn partial_cmp(&self, other: &Rev<T>) -> Option<Ordering> {
+        other.0.partial_cmp(&self.0)
+    }
+}
+
+impl<T: Ord> Ord for Rev<T> {
+    fn cmp(&self, other: &Rev<T>) -> Ordering {
+        other.0.cmp(&self.0)
+    }
+}
+// 逆順ソートここまで
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 fn resolve() {
     unimplemented!();
