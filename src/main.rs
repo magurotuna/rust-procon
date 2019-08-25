143a14a1f2a256c7f69dad24ab524dbfb1e7040e
Yusuke Tanaka <wing0920@gmail.com>
2019-08-25 22:40:37 +0900

    むり Rust、再帰関数まわりがまず面倒というのと、連結成分を求める方法についての理解がまだまだ不足している

--- src/main.rs
+++ src/main.rs
@@ -42,5 +42,53 @@ fn rl() -> String {
 }
 
 fn main() {
-    unimplemented!();
+    let (n, m) = read!(usize, usize);
+    let input: Vec<(usize, usize)> = read![usize, usize; m];
+    let input: Vec<(usize, usize)> = input.iter().map(|&x| (x.0 - 1, x.1 - 1)).collect(); // 0オリジンに直す
+
+    // i番目の頂点を頂点iと呼ぶことにする
+    // 頂点iから頂点x, yに辺が伸びている場合は graph[i] = vec![x, y] であるとする
+    let mut graph: Vec<Vec<usize>> = vec![Vec::new(); n];
+    for i in 0..m {
+        let (from, to) = input[i];
+        graph[from].push(to);
+    }
+
+    // 通過していない頂点の番号を格納するベクタ
+    let mut points: Vec<usize> = (0..n).rev().collect();
+
+    let mut ans = 0;
+
+    // 全頂点を通過するまでループ
+    while !points.is_empty() {
+        let p = points.pop().unwrap();
+        let ne = &graph[p];
+        fn dfs(cur: usize, neighbor: &Vec<usize>, ps: &Vec<usize>, g: &Vec<Vec<usize>>) -> bool {
+            if ps.contains(&cur) {
+                return false;
+            }
+            let mut ret = true;
+            for nei in neighbor {
+                if ps.contains(nei) {
+                    ret = false;
+                }
+                // nei を通過済みとする
+                let ps = ps
+                    .iter()
+                    .filter_map(|&x| if x != *nei { Some(x) } else { None })
+                    .collect();
+
+                // cur = nei, neighbor = graph[nei] として再帰
+                ret = dfs(*nei, &g[*nei], &ps, g);
+            }
+            ret
+        }
+        ans = ans
+            + if dfs(p, ne, &mut points, &graph) {
+                1
+            } else {
+                0
+            };
+    }
+    println!("{}", &ans);
 }
