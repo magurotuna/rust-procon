01a6d6c356979974ec4044433ec77925d543ed23
Yusuke Tanaka <wing0920@gmail.com>
2019-08-26 23:03:30 +0900

    AC 3ms

--- src/main.rs
+++ src/main.rs
@@ -42,5 +42,47 @@ fn rl() -> String {
 }
 
 fn main() {
-    unimplemented!();
+    let (h, w): (i64, i64) = read!(i64, i64);
+
+    let mut ans = INF;
+    for i in 1..(w + h - 1) {
+        // i <= w - 1 のときは x = i のところで1つ目の区切りを入れる
+        if i <= w - 1 {
+            let first_s = i * h;
+
+            // 2つ目の区切りは縦方向の中央に入れるか横方向の中央に入れるかの2通り
+            // 横方向に区切りを入れる場合
+            let second_s_h = (w - i) * (h / 2);
+            let third_s_h = h * w - first_s - second_s_h;
+            // 縦方向に区切りを入れる場合
+            let second_s_v = h * ((w - i) / 2);
+            let third_s_v = h * w - first_s - second_s_v;
+
+            let s_diff_h =
+                max(first_s, max(second_s_h, third_s_h)) - min(first_s, min(second_s_h, third_s_h));
+            let s_diff_v =
+                max(first_s, max(second_s_v, third_s_v)) - min(first_s, min(second_s_v, third_s_v));
+            let s_diff = min(s_diff_h, s_diff_v);
+            ans = if s_diff < ans { s_diff } else { ans };
+        } else {
+            // i >= w のときは y = i - w + 1 のところで1つ目の区切りを入れる
+            let first_s = (i - w + 1) * w;
+
+            // 2つ目の区切りは縦方向の中央に入れるか横方向の中央に入れるかの2通り
+            // 横方向に区切りを入れる場合
+            let second_s_h = w * ((h - i + w - 1) / 2);
+            let third_s_h = h * w - first_s - second_s_h;
+            // 縦方向に区切りを入れる場合
+            let second_s_v = (h - i + w - 1) * (w / 2);
+            let third_s_v = h * w - first_s - second_s_v;
+
+            let s_diff_h =
+                max(first_s, max(second_s_h, third_s_h)) - min(first_s, min(second_s_h, third_s_h));
+            let s_diff_v =
+                max(first_s, max(second_s_v, third_s_v)) - min(first_s, min(second_s_v, third_s_v));
+            let s_diff = min(s_diff_h, s_diff_v);
+            ans = if s_diff < ans { s_diff } else { ans };
+        }
+    }
+    println!("{}", &ans);
 }
