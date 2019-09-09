45d2768b5d7847e9313ef7bae798a1a535e0c7b6
Yusuke Tanaka <wing0920@gmail.com>
2019-09-09 22:57:10 +0900

    貪欲？っぽい方法ためしたけど実装しきれなかった

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,75 @@ macro_rules! debug {
     }
 }
 
+#[derive(PartialEq, PartialOrd)]
+pub struct OrdFloat<T>(pub T);
+
+impl<T: PartialEq> Eq for OrdFloat<T> {}
+
+impl<T: PartialOrd> Ord for OrdFloat<T> {
+    fn cmp(&self, other: &OrdFloat<T>) -> Ordering {
+        self.0.partial_cmp(&other.0).unwrap()
+    }
+}
+
 fn main() {
-    unimplemented!();
+    let (d, g) = read!(i32, i32);
+    let pc: Vec<(i32, i32)> = read![i32, i32; d];
+
+    // (問題数, コンプリートボーナス, コンプリートボーナスなしの場合の1問あたりの点数, ありの場合の1問あたりの点数)
+    let mut pc: Vec<_> = pc
+        .into_iter()
+        .enumerate()
+        .map(|(i, (x, y))| {
+            let p_per_q = (i as i32 + 1) * 100;
+            let comp_point = p_per_q * x + y;
+            let point_per_q = comp_point as f64 / x as f64;
+            (x, y, p_per_q, point_per_q)
+        })
+        .collect();
+
+    let mut pc2 = pc.clone();
+
+    // ボーナスありでの1問あたりの点数によるソート
+    pc.sort_by_key(|x| OrdFloat(x.3));
+    // ボーナスなしでの1問あたりの点数によるソート（自明だけど..）
+    pc2.sort_by_key(|x| x.2);
+
+    debug!(pc);
+    debug!(pc2);
+
+    let mut rest: i32 = g;
+    let mut ans = 0;
+
+    while rest > 0 {
+        let &last = pc.last().unwrap();
+        let all_point = last.2 * last.0 + last.1;
+        if rest - all_point >= 0 {
+            rest -= all_point;
+            ans += last.0;
+            pc.pop();
+            pc2 = pc2.into_iter().filter(|x| x.2 != last.2).collect();
+            continue;
+        } else {
+            let last2 = pc2.pop().unwrap();
+
+            let num = if rest % last2.2 == 0 {
+                rest / last2.2
+            } else {
+                (rest / last2.2) + 1
+            };
+
+            if num <= last2.0 {
+                ans += min(num, last.0);
+                break;
+            } else {
+                rest -= last2.2 * last2.0 + last2.1;
+                ans += num;
+                pc = pc.into_iter().filter(|x| x.2 != last2.2).collect();
+                continue;
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
