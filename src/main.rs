a5feae700f160b4e7668275e418e9299dc15f47c
Yusuke Tanaka <wing0920@gmail.com>
2019-09-14 21:15:28 +0900

    AC 44ms

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,98 @@ macro_rules! debug {
     }
 }
 
+struct DisjointSet {
+    parent: Vec<usize>,
+    rank: Vec<usize>,
+}
+
+impl DisjointSet {
+    fn new(n: usize) -> Self {
+        DisjointSet {
+            parent: (0..n).collect(),
+            rank: vec![0; n],
+        }
+    }
+
+    fn find(&mut self, x: usize) -> Option<usize> {
+        if x > self.parent.len() {
+            None
+        } else if self.parent[x] == x {
+            Some(x)
+        } else {
+            let px = self.parent[x];
+            let root = self.find(px).unwrap(); // AtCoderでのRustバージョンではpxを分離しないとだめ
+            self.parent[x] = root;
+            Some(root)
+        }
+    }
+
+    pub fn unite(&mut self, x: usize, y: usize) {
+        let x_root = match self.find(x) {
+            None => return,
+            Some(val) => val,
+        };
+        let y_root = match self.find(y) {
+            None => return,
+            Some(val) => val,
+        };
+
+        if x_root == y_root {
+            return;
+        }
+
+        if self.rank[x] < self.rank[y] {
+            self.parent[x_root] = y_root;
+        } else {
+            self.parent[y_root] = x_root;
+            if self.rank[x_root] == self.rank[y_root] {
+                self.rank[x_root] += 1;
+            }
+        }
+    }
+
+    pub fn same(&mut self, x: usize, y: usize) -> bool {
+        let x_root = match self.find(x) {
+            None => return false,
+            Some(val) => val,
+        };
+        let y_root = match self.find(y) {
+            None => return false,
+            Some(val) => val,
+        };
+
+        x_root == y_root
+    }
+}
+
 fn main() {
-    unimplemented!();
+    let (n, m) = read!(usize, usize);
+    let p: Vec<usize> = read![[usize]];
+    let xy: Vec<(usize, usize)> = read!(usize, usize; m);
+
+    let p = p.into_iter().map(|x| x - 1).collect::<Vec<_>>();
+    // 数字jがp[i]に入っているとき pos[j] := i とする
+    let mut pos = vec![0; n];
+    for i in 0..n {
+        pos[p[i]] = i;
+    }
+
+    let xy = xy
+        .into_iter()
+        .map(|x| {
+            if x.0 > x.1 {
+                (x.1 - 1, x.0 - 1)
+            } else {
+                (x.0 - 1, x.1 - 1)
+            }
+        })
+        .collect::<Vec<(usize, usize)>>();
+
+    let mut ds = DisjointSet::new(n);
+    for (x, y) in xy {
+        ds.unite(x, y);
+    }
+
+    let ans = (0..n).filter(|&x| ds.same(x, pos[x])).count();
+    println!("{}", ans);
 }
