6753bd0ba1352d7344f5255dd1f75a626f68ac44
Yusuke Tanaka <wing0920@gmail.com>
2019-09-16 10:39:05 +0900

    AC 16ms

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,46 @@ macro_rules! debug {
     }
 }
 
+pub fn gcd(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+pub fn lcm(a: u64, b: u64) -> u64 {
+    a / gcd(a, b) * b
+}
+
 fn main() {
-    unimplemented!();
+    let (n, m) = read!(usize, usize);
+    let s: String = read!(String);
+    let t: String = read!(String);
+
+    let sc: Vec<char> = s.chars().collect();
+    let tc: Vec<char> = t.chars().collect();
+
+    // good stringが存在するならばその長さの最小はnとmの最小公倍数に等しい（最小公倍数より大きい場合はXの中にSを作るのにもTを作るのにも使われない文字が存在することになるため）
+
+    let lc = lcm(n as u64, m as u64) as usize;
+
+    let mut x_hm = HashMap::new();
+    for i in 0..sc.len() {
+        x_hm.insert(i * lc / n, sc[i]);
+    }
+    for j in 0..tc.len() {
+        let index = j * lc / m;
+        match x_hm.get(&index) {
+            Some(&v) => {
+                if v != tc[j] {
+                    println!("-1");
+                    return;
+                }
+            }
+            None => (),
+        }
+    }
+
+    println!("{}", lc);
 }
