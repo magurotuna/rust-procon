15c79753d5ca8fd2caa5a8ea832c493039d1be45
Yusuke Tanaka <wing0920@gmail.com>
2019-09-24 00:57:54 +0900

    AC 3ms

--- src/main.rs
+++ src/main.rs
@@ -49,6 +49,79 @@ macro_rules! debug {
     }
 }
 
+pub trait Prime {
+    fn lower_primes(&self) -> Vec<usize>;
+    fn factorize(&self) -> HashMap<usize, usize>;
+}
+
+impl Prime for usize {
+    /// エラトステネスの篩を用いてself以下の素数を求める
+    fn lower_primes(&self) -> Vec<usize> {
+        let &this = self;
+        let mut v = Vec::new();
+        if this < 2 {
+            return v;
+        }
+        let mut deque = (2..(this + 1)).collect::<VecDeque<usize>>();
+
+        let mut p = match deque.pop_front() {
+            Some(x) => x,
+            None => return v,
+        };
+        v.push(p);
+        while p as f64 <= (this as f64).sqrt() {
+            deque = deque.iter().filter(|&&x| x % p != 0).map(|x| *x).collect();
+            p = match deque.pop_front() {
+                Some(x) => x,
+                None => return v,
+            };
+            v.push(p);
+        }
+        for n in deque {
+            v.push(n);
+        }
+        v
+    }
+
+    /// エラトステネスの篩を用いてselfを素因数分解する
+    fn factorize(&self) -> HashMap<usize, usize> {
+        let mut ret = HashMap::new();
+        let primes = ((*self as f64).sqrt() as usize).lower_primes();
+
+        let mut tmp = *self;
+        for prime in primes {
+            while tmp % prime == 0 {
+                tmp /= prime;
+                *ret.entry(prime).or_insert(0) += 1;
+            }
+        }
+        if tmp > 1 {
+            *ret.entry(tmp).or_insert(0) += 1;
+        }
+        ret
+    }
+}
+
 fn main() {
-    unimplemented!();
+    let n: usize = read!(usize);
+    let M = MOD_10_9_7 as usize;
+
+    if n == 1 {
+        println!("1");
+        return;
+    }
+
+    let mut h = HashMap::new();
+
+    for i in 2..(n + 1) {
+        let hm = i.factorize();
+        for (k, v) in hm {
+            *h.entry(k).or_insert(0) += v;
+        }
+    }
+
+    println!(
+        "{}",
+        h.values().map(|&x| x + 1).fold(1usize, |t, v| (t * v) % M)
+    );
 }
