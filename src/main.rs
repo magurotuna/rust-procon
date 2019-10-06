8245a5d58923037ea29213067d17d558e304f51d
Yusuke Tanaka <wing0920@gmail.com>
2019-10-06 22:55:31 +0900

    Aだけ

--- src/main.rs
+++ src/main.rs
@@ -52,6 +52,94 @@ macro_rules! debugln {
     ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
 }
 
-fn main() {
+fn a() {
+    let s: String = read!(String);
+    let k: usize = read!(usize);
+
+    let c: Vec<char> = s.chars().collect();
+    // ランレングス圧縮
+    let mut runlen = Vec::new();
+    let mut prev_c = c[0];
+    let mut tmp = 0;
+    for i in 0..c.len() {
+        if prev_c == c[i] {
+            tmp += 1;
+            continue;
+        } else {
+            prev_c = c[i];
+            runlen.push(tmp);
+            tmp = 1;
+        }
+    }
+    runlen.push(tmp);
+
+    let mut ans = 0;
+    // 先頭と末尾が同じ文字かどうかで場合分け
+    if c.first().unwrap() != c.last().unwrap() {
+        // 先頭と末尾が違う場合
+        for r in runlen {
+            if r == 1 {
+                continue;
+            } else {
+                ans += r / 2;
+            }
+        }
+        ans *= k;
+    } else {
+        // 先頭と末尾が同じ場合
+        if runlen.len() > 1 {
+            let r1 = &runlen[0];
+            let r_middle = runlen.get(1..(runlen.len() - 1));
+            let r2 = &runlen[runlen.len() - 1];
+
+            match r_middle {
+                Some(v) => {
+                    for &r in v {
+                        if r >= 2 {
+                            ans += r / 2;
+                        }
+                    }
+                    ans *= k;
+                }
+                None => (),
+            }
+
+            ans += *r1 / 2;
+            ans += *r2 / 2;
+
+            ans += (k - 1) * ((*r1 + *r2) / 2);
+        } else {
+            ans += (runlen[0] * k) / 2;
+        }
+    }
+    println!("{}", ans);
+}
+
+fn b() {
+    let n = read!(usize);
+    let mut s = vec![];
+    for i in 0..n {
+        let t: Vec<i32> = read![[i32]];
+        s.push(t);
+    }
+}
+
+fn c() {
     unimplemented!();
+}
+
+fn d() {
+    unimplemented!();
+}
+
+fn e() {
+    unimplemented!();
+}
+
+fn f() {
+    unimplemented!();
+}
+
+fn main() {
+    b();
 }
