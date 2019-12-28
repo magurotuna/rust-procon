07d40a6bea53f09167beef3803b3cec1ce4b9662
Yusuke Tanaka <wing0920@gmail.com>
2019-12-28 14:26:37 +0900

    AC 8ms

--- src/main.rs
+++ src/main.rs
@@ -52,6 +52,127 @@ macro_rules! debugln {
     ($($format:tt)*) => (writeln!(std::io::stderr(), $($format)*).unwrap());
 }
 
+pub trait LexicalPermutation {
+    /// Return `true` if the slice was permuted, `false` if it is already
+    /// at the last ordered permutation.
+    fn next_permutation(&mut self) -> bool;
+    /// Return `true` if the slice was permuted, `false` if it is already
+    /// at the first ordered permutation.
+    fn prev_permutation(&mut self) -> bool;
+}
+
+impl<T> LexicalPermutation for [T]
+where
+    T: Ord,
+{
+    /// Original author in Rust: Thomas Backman <serenity@exscape.org>
+    fn next_permutation(&mut self) -> bool {
+        // These cases only have 1 permutation each, so we can't do anything.
+        if self.len() < 2 {
+            return false;
+        }
+
+        // Step 1: Identify the longest, rightmost weakly decreasing part of the vector
+        let mut i = self.len() - 1;
+        while i > 0 && self[i - 1] >= self[i] {
+            i -= 1;
+        }
+
+        // If that is the entire vector, this is the last-ordered permutation.
+        if i == 0 {
+            return false;
+        }
+
+        // Step 2: Find the rightmost element larger than the pivot (i-1)
+        let mut j = self.len() - 1;
+        while j >= i && self[j] <= self[i - 1] {
+            j -= 1;
+        }
+
+        // Step 3: Swap that element with the pivot
+        self.swap(j, i - 1);
+
+        // Step 4: Reverse the (previously) weakly decreasing part
+        self[i..].reverse();
+
+        true
+    }
+
+    fn prev_permutation(&mut self) -> bool {
+        // These cases only have 1 permutation each, so we can't do anything.
+        if self.len() < 2 {
+            return false;
+        }
+
+        // Step 1: Identify the longest, rightmost weakly increasing part of the vector
+        let mut i = self.len() - 1;
+        while i > 0 && self[i - 1] <= self[i] {
+            i -= 1;
+        }
+
+        // If that is the entire vector, this is the first-ordered permutation.
+        if i == 0 {
+            return false;
+        }
+
+        // Step 2: Reverse the weakly increasing part
+        self[i..].reverse();
+
+        // Step 3: Find the rightmost element equal to or bigger than the pivot (i-1)
+        let mut j = self.len() - 1;
+        while j >= i && self[j - 1] < self[i - 1] {
+            j -= 1;
+        }
+
+        // Step 4: Swap that element with the pivot
+        self.swap(i - 1, j);
+
+        true
+    }
+}
+
 fn main() {
-    unimplemented!();
+    let n = read!(usize);
+    let mut xy = Vec::with_capacity(n);
+    for _ in 0..n {
+        let a = read!(i32, i32);
+        xy.push(Position { x: a.0, y: a.1 });
+    }
+    let mut cities: Vec<usize> = (0..n).collect();
+    let mut perms = Vec::new();
+
+    loop {
+        perms.push(cities.clone());
+        if !cities.next_permutation() {
+            break;
+        }
+    }
+
+    let cnt = perms.len() as f64;
+    let s: f64 = perms
+        .into_iter()
+        .map(|p| {
+            let mut d = 0.0;
+            for i in 0..(n - 1) {
+                let from = p[i];
+                let to = p[i + 1];
+                d += dist(xy[from], xy[to]);
+            }
+            d
+        })
+        .sum();
+
+    println!("{}", s / cnt);
+}
+
+#[derive(Clone, Copy)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+fn dist(from: Position, to: Position) -> f64 {
+    let xx = ((from.x - to.x) as f64).powi(2);
+    let yy = ((from.y - to.y) as f64).powi(2);
+    (xx + yy).sqrt()
 }
